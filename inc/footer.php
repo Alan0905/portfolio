9e58554b4e23b2bd24b6aa3169c9daf73d15b44a
Alan0905 <negrette.alanet36@gmail.com>
2022-11-29 23:46:07 -0300

    cambios para github pages

---
+++ inc/footer.php
@@ -0,0 +1,32 @@
+ <!-- footer
+   ================================================== -->
+   <footer>
+
+      <div class="row">
+
+         <div class="twelve columns">
+
+            <ul class="social-links">
+               <li><a target ="_blank" href="<?php echo $contact['linkin'] ?>"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+
+
+         </div>
+
+         <div id="go-top"><a class="smoothscroll" title="Back to Top" href="#home"><i class="icon-up-open"></i></a></div>
+
+      </div>
+
+   </footer> <!-- Footer End-->
+
+   <!-- Java Script
+   ================================================== -->
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+   <script>window.jQuery || document.write('<script src="<?php echo base_url ?>profile_asset/js/jquery-1.10.2.min.js"><\/script>')</script>
+   <script type="text/javascript" src="<?php echo base_url ?>profile_asset/js/jquery-migrate-1.2.1.min.js"></script>
+
+   <script src="<?php echo base_url ?>profile_asset/js/jquery.flexslider.js"></script>
+   <script src="<?php echo base_url ?>profile_asset/js/waypoints.js"></script>
+   <script src="<?php echo base_url ?>profile_asset/js/jquery.fittext.js"></script>
+   <script src="<?php echo base_url ?>profile_asset/js/magnific-popup.js"></script>
+   <script src="<?php echo base_url ?>profile_asset/js/init.js"></script>
