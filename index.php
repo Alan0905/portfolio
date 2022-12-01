eeaf53e49ef8d2360742b93b8e0e8e757d1d6300
Alan0905 <negrette.alanet36@gmail.com>
2022-12-01 14:22:33 -0300

    Formulario contacto y arreglo de url

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php require_once('config.php'); ?>
  <!DOCTYPE html>
 <html lang="en" class="" style="height: auto;">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <?php require_once('inc/header.php') ?>
   <body>
 
@@ -210,7 +211,7 @@ while($row = $c_qry->fetch_assoc()){
 
          <div class="twelve columns collapsed">
 
-            <h1>Proyectos que realice durante mi carreraaaaaaaaaaaaaa</h1>
+            <h1>Proyectos que realice durante mi carrera</h1>
 
             <!-- portfolio-wrapper -->
             <div id="portfolio-wrapper" class="bgrid-quarters s-bgrid-thirds cf">
@@ -270,6 +271,39 @@ while($row = $c_qry->fetch_assoc()){
 
 
       </div> <!-- row End -->
+
+      
+
+      <div class="container">
+         <BR>
+        <h1 class="text-center">CONTACTAME!</h1>
+        <hr>
+        <form action="enviar.php" method="post">
+            <div class="mb-3">
+                <label for="nombre" class="form-label">Nombre</label>
+                <input type="text" class="form-control" name="nombre" id="nombre" required>
+              </div>
+              <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="correo" placeholder="correo@example.com" required>
+              </div>
+              <div class="mb-3">
+                <label for="mensaje" class="form-label">Mensaje</label>
+                <textarea class="form-control" id="mensaje" name="mensaje" rows="4" required></textarea>
+              </div>
+              <div class="d-grid gap-2 col-10 mx-auto">
+              <button type="submit" class="btn btn-outline-success ">Enviar</button>
+            </div>
+        </form>
+        
+
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
 
    </section> <!-- Portfolio Section End-->
 
